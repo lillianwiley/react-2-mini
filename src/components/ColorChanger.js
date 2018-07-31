d085b11d29cda0e21155a31f93c98d6aa03cb8e4
Lillian Wiley <lillianwiley92@gmail.com>
2018-07-30 19:39:58 -0600

    completed react2 mini

--- src/components/ColorChanger.js
+++ src/components/ColorChanger.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-export default function ColorChanger() {
+export default function ColorChanger(props) {
   return (
-    <select className="dropDownContainer">
+    <select className="dropDownContainer" onChange={(e) => props.update(e.target.value)} disabled={props.allowEdit === 'false'}>
       <option value="black"> Black </option>
       <option value="blue"> Blue </option>
       <option value="green"> Green </option>
     </select>
   )
 }
+
+
+// //function add(obj){
+// var total = obj.num500 + obj.num90
+// }
+
+// // let numbers = {
+// //   num1: 2,
+// //   num2: 4
+// // }
+// add({num500: 3, num90: 4})
